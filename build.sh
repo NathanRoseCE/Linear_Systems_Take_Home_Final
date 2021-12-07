11f056fbaeb8f2fce7e42f3bf696eda4dd43f5fc
Nathan Rose <mail.nathanrose@gmail.com>
2021-12-06 22:59:24 -0500

    Created a quiet mode for pdflatex that is on by default rn

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-
+quietmode=true
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
@@ -9,10 +9,18 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 cd scripts
 
-python3 Main.py
+echo "Building Artifacts, this may take a second"
 
 cd ..
 
 # bazel build ee510final
-pdflatex -shell-escape Final.tex
-pdflatex -shell-escape Final.tex
+echo "Building PDF document"
+if [ "$quietmode" = true ] ; then
+    python3 Main.py 
+    pdflatex -shell-escape -halt-on-error Final.tex > /dev/null 2>&1
+    pdflatex -shell-escape -halt-on-error Final.tex > /dev/null 2>&1
+else
+    python3 Main.py
+    pdflatex -shell-escape -halt-on-error Final.tex 
+    pdflatex -shell-escape -halt-on-error Final.tex
+fi
