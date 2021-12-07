f3e5ea7ea17a0278532ff937d79f6213620837cf
Nathan Rose <mail.nathanrose@gmail.com>
2021-12-06 23:04:50 -0500

    oops it didnt work, now it does

--- build.sh
+++ build.sh
@@ -7,20 +7,22 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
-cd scripts
 
-echo "Building Artifacts, this may take a second"
-
-cd ..
-
-# bazel build ee510final
-echo "Building PDF document"
 if [ "$quietmode" = true ] ; then
-    python3 Main.py 
-    pdflatex -shell-escape -halt-on-error Final.tex > /dev/null 2>&1
-    pdflatex -shell-escape -halt-on-error Final.tex > /dev/null 2>&1
+    echo "Building Artifacts, this may take a second"
+    cd scripts
+    python3 Main.py > /dev/null 2>&1
+    cd ..
+    echo "Building PDF"
+    pdflatex -shell-escape Final.tex > /dev/null 2>&1
+    pdflatex -shell-escape Final.tex > /dev/null 2>&1
 else
-    python3 Main.py
-    pdflatex -shell-escape -halt-on-error Final.tex 
-    pdflatex -shell-escape -halt-on-error Final.tex
+    echo "Building Artifacts, this may take a second"
+    cd scripts
+    python3 Main.py 
+    cd ..
+    echo "Building PDF"
+    pdflatex -shell-escape Final.tex 
+    pdflatex -shell-escape Final.tex
 fi
+echo "Success"
